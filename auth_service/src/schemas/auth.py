12e65b6acddd27f133bb1e29ddabb42de6b0355e
Sergey-1214 <serezha.pavlov.06@gmail.com>
2026-07-30 16:17:17 +0300

    feat: add pydantic schemas

---
+++ auth_service/src/schemas/auth.py
@@ -0,0 +1,42 @@
+
+
+from datetime import datetime
+import uuid
+
+from pydantic import BaseModel, EmailStr, SecretStr
+
+
+class RegisterRequest(BaseModel):
+    username: str
+    email: EmailStr
+    password: SecretStr
+
+class LoginRequest(BaseModel):
+    username: str
+    email: EmailStr
+    password: SecretStr
+
+class UserDTO(BaseModel):
+    id: uuid.UUID
+    
+    username: str
+    email: str
+
+    created_at: datetime
+
+class User(UserDTO):
+    hash_password: str
+
+
+class TokenPair(BaseModel):
+    access_token: str
+    refresh_token: str
+    refresh_token_expires_at: datetime
+    
+
+class RefreshTokenRequest(BaseModel):
+    refresh_token: str
+
+
+class LogoutRequest(BaseModel):
+    refresh_token: str
