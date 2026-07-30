83d03931b2f4934e1b1da193007f462bb04eb879
Sergey-1214 <serezha.pavlov.06@gmail.com>
2026-07-30 16:20:34 +0300

    feat: add exceptions and handler for app exceptions

---
+++ auth_service/src/exceptions/auth.py
@@ -0,0 +1,29 @@
+
+from fastapi import status
+
+class AppException(Exception):
+    def __init__(self, status_code: str, detail: str):
+        self.status_code=status_code
+        self.detail = detail
+        
+
+class UserAlreadyExistsException(AppException):
+    def __init__(self, username: str, email: str):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"User with email: {email} and username: {username} already exsist"
+        )
+
+class UnauthorizedException(AppException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"User unauthorized"
+        )
+
+class UserNotFoundException(AppException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
