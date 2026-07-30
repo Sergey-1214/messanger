b27643a5b1cd967ce74ac6ae6a32f14b9e7ddcf5
Sergey-1214 <serezha.pavlov.06@gmail.com>
2026-07-30 16:28:31 +0300

    feat: add Dockerfile, requirements and main for start project

---
+++ auth_service/src/main.py
@@ -0,0 +1,27 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from src.db.database import engine
+from src.handler.auth import auth_router
+from src.core.logging import setup_logging
+from src.db.database import Base, dispose_db_engine
+from src.exceptions.auth import UnauthorizedException, UserAlreadyExistsException, UserNotFoundException
+from src.exceptions.handler import unauthorized_handler, user_already_exsists_handler, user_not_found_handler
+
+setup_logging()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await dispose_db_engine()
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(auth_router)
+app.add_exception_handler(UserAlreadyExistsException, user_already_exsists_handler)
+app.add_exception_handler(UserNotFoundException, user_not_found_handler)
+app.add_exception_handler(UnauthorizedException, unauthorized_handler)
