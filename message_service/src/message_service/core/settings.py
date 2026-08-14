7f055f89739e69d9c223868d7447f81425f3438c
Sergey-1214 <serezha.pavlov.06@gmail.com>
2026-08-14 14:52:15 +0300

    feat: add rabbitmqurl to settingsd

--- message_service/src/message_service/core/settings.py
+++ message_service/src/message_service/core/settings.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     database_url: str
     auth_service_url: str = "http://auth_service:8000"
+    rabbitmq_url: str = "amqp://app:app-password@localhost:5672/"
 
     model_config = SettingsConfigDict(env_file=".env")
 
