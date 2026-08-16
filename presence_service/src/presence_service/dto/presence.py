54bc3e245bfc5dc12bf9047ca5bd9ad0e32d9720
Sergey-1214 <serezha.pavlov.06@gmail.com>
2026-08-16 14:17:02 +0300

    feat: add enum for heartbeat response

--- presence_service/src/presence_service/dto/presence.py
+++ presence_service/src/presence_service/dto/presence.py
@@ -1,8 +1,14 @@
 
 from dataclasses import dataclass
+from enum import Enum, IntEnum
 
 
 @dataclass(frozen=True, slots=True)
 class AddConnectionResult:
     status_changed: bool
     active_connections: int
+
+
+class HeartbeatResult(IntEnum):
+    CONNECTION_NOT_FOUND = 0
+    OK = 1
