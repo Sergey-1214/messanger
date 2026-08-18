2fea77853f6eaac650e072f4aeeb289349a2707c
Sergey-1214 <serezha.pavlov.06@gmail.com>
2026-08-18 16:38:17 +0300

    feat: add new handlers for message update and delete events

--- realtime_gateway/src/realtime_gateway/handlers/message_events.py
+++ realtime_gateway/src/realtime_gateway/handlers/message_events.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from realtime_gateway.connections.manager import ConnectionManager
-from realtime_gateway.dto.events.message_created import MessageCreatedEvent
+from realtime_gateway.dto.events.message_created import MessageCreatedEvent, MessageDeletedEvent, MessageUpdatedEvent
 from realtime_gateway.schemas.websocket import ServerEvent
 
 class MessageEventsHandler:
@@ -26,5 +26,44 @@ class MessageEventsHandler:
                 for participant_id in set(event.chat_participants)
             )
         )
+
+    async def handle_message_updated(self, event: MessageUpdatedEvent):
+        websocket_event = ServerEvent(
+            type="message.updated",
+            request_id=event.correlation_id,
+            payload={
+                "message": event.message.model_dump(mode="json"),
+            },
+        ).model_dump(mode="json")
+
+        await asyncio.gather(
+            *(
+                self._connections.send_to_user(
+                    user_id=participant_id,
+                    event=websocket_event,
+                )
+                for participant_id in set(event.chat_participants)
+            )
+        )
+
+    async def handle_message_deleted(self, event: MessageDeletedEvent):
+        websocket_event = ServerEvent(
+            type="message.deleted",
+            request_id=event.correlation_id,
+            payload={
+                "message": event.message.model_dump(mode="json"),
+            },
+        ).model_dump(mode="json")
+
+        await asyncio.gather(
+            *(
+                self._connections.send_to_user(
+                    user_id=participant_id,
+                    event=websocket_event,
+                )
+                for participant_id in set(event.chat_participants)
+            )
+        )
+    
 
     
