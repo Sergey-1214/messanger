ec510fa2b799c43e02b1a3e5484707d4adcbd851
Sergey-1214 <serezha.pavlov.06@gmail.com>
2026-08-18 19:30:03 +0300

    feat: configure database and database sessions

---
+++ user_service/src/user_service/db/db.py
@@ -0,0 +1,34 @@
+
+
+from typing import AsyncGenerator
+
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from user_service.core.settings import settings
+
+
+engine = create_async_engine(
+    url=settings.DATABASE_URL,
+    pool_pre_ping=True,
+)
+
+async_session_maker = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def dispose_db_engine() -> None:
+    await engine.dispose()
