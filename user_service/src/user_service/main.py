4facd1940ee11f0dbed036006605c6608a80f019
Sergey-1214 <serezha.pavlov.06@gmail.com>
2026-08-19 12:54:19 +0300

    feat: register user not found exception handler

--- user_service/src/user_service/main.py
+++ user_service/src/user_service/main.py
@@ -4,8 +4,8 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from user_service.exceptions.handler import user_already_exist_handler
-from user_service.exceptions.user import UserAlreadyExistException
+from user_service.exceptions.handler import user_already_exist_handler, user_not_found_handler
+from user_service.exceptions.user import UserAlreadyExistException, UserNotFoundException
 from user_service.handler.user import router as user_router
 from user_service.db.db import Base, dispose_db_engine, engine
 
@@ -23,3 +23,4 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(user_router)
 
 app.add_exception_handler(UserAlreadyExistException, user_already_exist_handler)
+app.add_exception_handler(UserNotFoundException, user_not_found_handler)
