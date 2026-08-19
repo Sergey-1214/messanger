21876d6fe886e56de27556574e19617d4302dc42
Sergey-1214 <serezha.pavlov.06@gmail.com>
2026-08-19 12:52:24 +0300

    feat: added handlers for get by id and username endpoints

--- user_service/src/user_service/schemas/user.py
+++ user_service/src/user_service/schemas/user.py
@@ -10,6 +10,8 @@ from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
 class CreateUserRequest(BaseModel):
     username: str = Field(min_length=2, max_length=100)
+    first_name: str | None = None
+    second_name: str | None = None
     email: EmailStr
     description: str | None = Field(default=None, min_length=1, max_length=500)
 
@@ -17,6 +19,8 @@ class CreateUserRequest(BaseModel):
 class User(BaseModel):
     id: UUID
     username: str 
+    first_name: str | None = Field(default=None, min_length=1, max_length=100)
+    second_name: str | None = Field(default=None, min_length=1, max_length=100)
     email: EmailStr
     description: str | None = Field(default=None, min_length=1, max_length=500)
 
