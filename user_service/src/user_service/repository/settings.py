ac05ca324a658f2cd49d356acbd792b9e1513141
Sergey-1214 <serezha.pavlov.06@gmail.com>
2026-08-20 12:57:24 +0300

    feat: added repository methods for get, update, clear setting

--- user_service/src/user_service/repository/settings.py
+++ user_service/src/user_service/repository/settings.py
@@ -1,10 +1,12 @@
 from uuid import UUID
 
 from fastapi import Depends
+from sqlalchemy import select, text, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from user_service.db.db import get_db_session
 from user_service.models.settings import Settings
+from user_service.models.user import User
 from user_service.repository.user import UserRepository
 
 
@@ -21,6 +23,39 @@ class SettingsRepository:
         await self.session.refresh(settings)
 
         return settings
+
+    async def get_user_settings(self, user_id: UUID):
+        stmt = select(Settings).join(User).where(User.id == user_id)
+
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def update_settings(
+        self, 
+        user_id: UUID, 
+        changes: dict,
+    ) -> Settings | None:
+        stmt = update(Settings)\
+                .values(**changes)\
+                .where(Settings.user_id == user_id)\
+                .returning(Settings)
+
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def clear_settings(self, user_id: UUID):
+        stmt = update(Settings)\
+                .where(Settings.user_id == user_id)\
+                .values(
+                    language=text("DEFAULT"),
+                    notification_enabled=text("DEFAULT"),
+                    last_seen_visibility=text("DEFAULT"),
+                    profile_photo_visibility=text("DEFAULT"),
+                ).returning(Settings)
+
+        result = await self.session.execute(stmt)
+
+        return result.scalar_one_or_none()
 
 
 def get_settings_repository(
