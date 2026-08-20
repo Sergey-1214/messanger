6c9baafda551559ebe893d9f528cc7091ebd1c23
Sergey-1214 <serezha.pavlov.06@gmail.com>
2026-08-20 13:02:18 +0300

    feat: register handler and include settings router

--- user_service/src/user_service/main.py
+++ user_service/src/user_service/main.py
@@ -4,9 +4,10 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from user_service.exceptions.handler import user_already_exist_handler, user_not_found_handler
-from user_service.exceptions.user import UserAlreadyExistException, UserNotFoundException
+from user_service.exceptions.handler import no_settings_changes_handler, user_already_exist_handler, user_not_found_handler
+from user_service.exceptions.user import NoSettingsChangesException, UserAlreadyExistException, UserNotFoundException
 from user_service.handler.user import router as user_router
+from user_service.handler.settings import router as settings_router
 from user_service.db.db import Base, dispose_db_engine, engine
 
 
@@ -21,6 +22,8 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(user_router)
+app.include_router(settings_router)
 
 app.add_exception_handler(UserAlreadyExistException, user_already_exist_handler)
 app.add_exception_handler(UserNotFoundException, user_not_found_handler)
+app.add_exception_handler(NoSettingsChangesException, no_settings_changes_handler)
